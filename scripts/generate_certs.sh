c4b469095b783755ed1946c447352e60319a765c
Alex Jones <tibbar@freedommail.ch>
2018-01-16 09:53:55 +0000

    working with dynamic keys

--- scripts/generate_certs.sh
+++
@@ -1,8 +0,0 @@
-if [ -d certs ]; then
-  rm -rf certs
-fi
-mkdir -p certs
-echo "make server cert"
-openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=foo@bar.com"
-echo "make client cert"
-openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress==foo@bar.com"
